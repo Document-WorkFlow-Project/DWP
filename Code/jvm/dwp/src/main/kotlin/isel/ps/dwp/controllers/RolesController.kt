1de7b9a16abb2b5a732571a612fd74317919f4cb
miguelalmeida2 <miguelalmeida991@protonmail.com>
2023-05-01 23:50:39 +0100

    Roles Controller to Services ✅

--- Code/jvm/dwp/src/main/kotlin/isel/ps/dwp/controllers/RolesController.kt
+++ Code/jvm/dwp/src/main/kotlin/isel/ps/dwp/controllers/RolesController.kt
@@ -2,16 +2,90 @@ package isel.ps.dwp.controllers
 
 import isel.ps.dwp.DwpApplication
 import isel.ps.dwp.database.jdbi.JdbiTransactionManager
+import isel.ps.dwp.model.Role
 import isel.ps.dwp.services.RoleServices
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
 
 @RestController
 @RequestMapping("/roles")
-class RolesController (
+class RolesController(
     private val roleServices: RoleServices = RoleServices(
         JdbiTransactionManager(jdbi = DwpApplication().jdbi())
     )
 ) {
 
+    @PostMapping("/")
+    fun createRole(@RequestBody role: RoleCreateModel, user: UserDetails): ResponseEntity<*> {
+        val res = roleServices.createRole(role.name, role.description)
+        return ResponseEntity
+            .status(201)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(res)
+    }
+
+    @DeleteMapping("/")
+    fun deleteRole(@RequestParam roleId: String, user: UserDetails): ResponseEntity<*> {
+        val res = roleServices.deleteRole(roleId)
+        return ResponseEntity
+            .status(200)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(res)
+    }
+
+    @PostMapping("/role/edit")
+    fun editRole(@RequestBody role: RoleEditModel, user: UserDetails): ResponseEntity<*> {
+        val res = roleServices.editRole(role.roleId, role.name, role.description)
+        return ResponseEntity
+            .status(200)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(res)
+    }
+
+    @GetMapping("/details")
+    fun roleDetails(@RequestParam roleId: String, user: UserDetails): ResponseEntity<*> {
+        val res = roleServices.roleDetails(roleId)
+        return ResponseEntity
+            .status(200)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(res)
+    }
+
+    @GetMapping("/")
+    fun rolesList(user: UserDetails): ResponseEntity<*> {
+        val res = roleServices.getRoles()
+        return ResponseEntity
+            .status(200)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(res)
+    }
+
+    @GetMapping("/role")
+    fun usersWithRole(@RequestParam roleId: String, user: UserDetails): ResponseEntity<*> {
+        val res = roleServices.getRoleUsers(roleId)
+        return ResponseEntity
+            .status(200)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(res)
+    }
+
+    @PutMapping("/user")
+    fun addRoleToUser(@RequestBody roleToAdd: RoleInPlayerModel, user: UserDetails): ResponseEntity<*> {
+        val res = roleServices.addRoleToUser(roleToAdd.roleId, roleToAdd.userId)
+        return ResponseEntity
+            .status(201)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(res)
+    }
+
+    @DeleteMapping("/user")
+    fun removeRoleFromUser(@RequestBody roleToDelete: RoleInPlayerModel, user: UserDetails): ResponseEntity<*> {
+        val res = roleServices.removeRoleFromUser(roleToDelete.roleId, roleToDelete.userId)
+        return ResponseEntity
+            .status(200)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(res)
+    }
 }
--- Code/jvm/dwp/src/main/kotlin/isel/ps/dwp/services/RoleServices.kt
+++ Code/jvm/dwp/src/main/kotlin/isel/ps/dwp/services/RoleServices.kt
@@ -1,42 +1,105 @@
 package isel.ps.dwp.services
 
+import isel.ps.dwp.ExceptionControllerAdvice
 import isel.ps.dwp.database.jdbi.TransactionManager
 import isel.ps.dwp.interfaces.RolesInterface
 import isel.ps.dwp.model.Role
 import org.springframework.stereotype.Component
 
 @Component
-class RoleServices(private val transactionManager: TransactionManager): RolesInterface {
-    override fun createRole(roleId: String) {
-        TODO("Not yet implemented")
+class RoleServices(private val transactionManager: TransactionManager) : RolesInterface {
+    override fun createRole(name: String, description: String): String {
+        if (name.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("Role Name can't be blank.")
+        if (description.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("Description can't be blank.")
+        if (name.length > 32)
+            throw ExceptionControllerAdvice.InvalidParameterException("Role Name length can't be bigger than 32 chars.")
+        if (description.length > 140)
+            throw ExceptionControllerAdvice.InvalidParameterException("Description length can't be bigger than 140 chars.")
+
+        return transactionManager.run {
+            val rolesRepo = it.rolesRepository
+            rolesRepo.createRole(name, description)
+        }
     }
 
     override fun deleteRole(roleId: String) {
-        TODO("Not yet implemented")
+        if (roleId.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("RoleId can't be blank.")
+
+        return transactionManager.run {
+            val rolesRepo = it.rolesRepository
+            rolesRepo.deleteRole(roleId)
+        }
     }
 
-    override fun editRole(roleId: String) {
-        TODO("Not yet implemented")
+    override fun editRole(roleId: String, name: String, description: String) {
+        if (roleId.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("RoleId can't be blank.")
+        if (name.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("Role Name can't be blank.")
+        if (description.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("Description can't be blank.")
+        if (name.length > 32)
+            throw ExceptionControllerAdvice.InvalidParameterException("New Role Name length can't be bigger than 32 chars.")
+        if (description.length > 140)
+            throw ExceptionControllerAdvice.InvalidParameterException("New Description length can't be bigger than 140 chars.")
+
+        return transactionManager.run {
+            val rolesRepo = it.rolesRepository
+            rolesRepo.editRole(roleId, name, description)
+        }
     }
 
     override fun roleDetails(roleId: String): Role {
-        TODO("Not yet implemented")
+        if (roleId.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("RoleId can't be blank.")
+
+        return transactionManager.run {
+            val rolesRepo = it.rolesRepository
+            rolesRepo.roleDetails(roleId)
+        }
     }
 
     override fun getRoles(): List<Role> {
-        TODO("Not yet implemented")
+        return transactionManager.run {
+            val rolesRepo = it.rolesRepository
+            rolesRepo.getRoles()
+        }
     }
 
     override fun getRoleUsers(roleId: String): List<String> {
-        TODO("Not yet implemented")
+        if (roleId.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("RoleId can't be blank.")
+
+        return transactionManager.run {
+            val rolesRepo = it.rolesRepository
+            rolesRepo.getRoleUsers(roleId)
+        }
     }
 
     override fun addRoleToUser(roleId: String, userId: String) {
-        TODO("Not yet implemented")
+        if (roleId.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("RoleId can't be blank.")
+        if (userId.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("UserId can't be blank.")
+
+        return transactionManager.run {
+            val rolesRepo = it.rolesRepository
+            rolesRepo.addRoleToUser(roleId, userId)
+        }
     }
 
     override fun removeRoleFromUser(roleId: String, userId: String) {
-        TODO("Not yet implemented")
-    }
+        if (roleId.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("RoleId can't be blank.")
+        if (userId.isBlank())
+            throw ExceptionControllerAdvice.ParameterIsBlank("UserId can't be blank.")
 
+        return transactionManager.run {
+            val rolesRepo = it.rolesRepository
+            rolesRepo.removeRoleFromUser(roleId, userId)
+        }
+    }
 }
