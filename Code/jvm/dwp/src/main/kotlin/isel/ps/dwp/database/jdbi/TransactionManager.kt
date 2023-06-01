3bea99b2b60023949623b4f8720da68736e5dc1c
miguelalmeida2 <miguelalmeida991@protonmail.com>
2023-06-01 23:01:13 +0100

    ADD: Set Isolation Level in Transactions

--- Code/jvm/dwp/src/main/kotlin/isel/ps/dwp/database/jdbi/TransactionManager.kt
+++ Code/jvm/dwp/src/main/kotlin/isel/ps/dwp/database/jdbi/TransactionManager.kt
@@ -1,5 +1,8 @@
 package isel.ps.dwp.database.jdbi
 
+import org.jdbi.v3.core.transaction.TransactionIsolationLevel
+
 interface TransactionManager {
     fun <R> run(block: (Transaction) -> R): R
+    fun <R> run(isolationLevel: TransactionIsolationLevel, block: (Transaction) -> R): R
 }
