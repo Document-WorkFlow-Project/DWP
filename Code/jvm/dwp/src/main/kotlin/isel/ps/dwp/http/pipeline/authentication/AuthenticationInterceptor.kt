52e21b6bbee7c98e8bedb5c571313a19d1597049
David Costa <a45935@alunos.isel.pt>
2023-06-02 14:43:59 +0100

    authenticationInterceptor fix

--- Code/jvm/dwp/src/main/kotlin/isel/ps/dwp/http/pipeline/authentication/AuthenticationInterceptor.kt
+++ Code/jvm/dwp/src/main/kotlin/isel/ps/dwp/http/pipeline/authentication/AuthenticationInterceptor.kt
@@ -5,6 +5,7 @@ import isel.ps.dwp.ExceptionControllerAdvice
 import isel.ps.dwp.model.UserAuth
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.jdbi.v3.core.Jdbi
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
@@ -13,36 +14,35 @@ import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
 class AuthenticationInterceptor(
-    private val authorizationHeaderProcessor: AuthorizationHeaderProcessor
+    private val authorizationHeaderProcessor: AuthorizationHeaderProcessor,
+    private val jdbi: Jdbi
 ) : HandlerInterceptor{
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        if (handler is HandlerMethod && handler.methodParameters.any { it.parameterType == UserAuth::class.java }
-        ) {
-            val bean = DwpApplication().jdbi()
-            val connection = bean.open().connection
 
-            connection.isClosed
-            if (connection.isValid(5)) {
-            } else {
-                throw ExceptionControllerAdvice.DatabaseIsNotAvailable("O Servidor não tem Acesso à DB")
+        jdbi.open().connection.use { connection ->
+
+            if (handler is HandlerMethod && handler.methodParameters.any { it.parameterType == UserAuth::class.java }) {
+                if (!connection.isValid(5))
+                    throw ExceptionControllerAdvice.DatabaseIsNotAvailable("O Servidor não tem Acesso à DB")
+
+                val user = if (request.getHeader(NAME_AUTHORIZATION_HEADER) != null)
+                    authorizationHeaderProcessor.process(request.getHeader(NAME_AUTHORIZATION_HEADER))
+                else
+                    authorizationHeaderProcessor.processCookie(request.getHeader(NAME_COOKIE_HEADER))
+
+                return if (user == null) {
+                    response.status = 401
+                    response.contentType = MediaType.APPLICATION_PROBLEM_JSON.type
+                    response.addHeader(NAME_WWW_AUTHENTICATE_HEADER, AuthorizationHeaderProcessor.SCHEME_BEARER)
+                    false
+                } else {
+                    UserArgumentResolver.addUserTo(user, request)
+                    true
+                }
             }
-            val user = if (request.getHeader(NAME_AUTHORIZATION_HEADER) != null) authorizationHeaderProcessor.process(
-                request.getHeader(NAME_AUTHORIZATION_HEADER)
-            ) else authorizationHeaderProcessor.processCookie(request.getHeader(NAME_COOKIE_HEADER))
-
-            return if (user == null) {
-                response.status = 401
-                response.contentType = MediaType.APPLICATION_PROBLEM_JSON.type
-                response.addHeader(NAME_WWW_AUTHENTICATE_HEADER, AuthorizationHeaderProcessor.SCHEME_BEARER)
-                false
-            } else {
-                UserArgumentResolver.addUserTo(user, request)
-                true
-            }
-
+            return true
         }
-        return true
     }
 
     companion object {
